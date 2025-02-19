2f611737e5270b2bb6f8173a49b81019f82bd268
DavidePafumi <119696808+DavidePafumi@users.noreply.github.com>
2025-02-19 06:16:49 +0000

    Deploying to gh-pages from @ DavidePafumi/DavidePafumi.github.io@c43d7fae25d1d49fffc1c89f78136402c57d117f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -396,8 +396,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-the-canterbury-tales-project",
+          title: 'The Canterbury Tales Project',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
